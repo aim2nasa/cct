f076e3ae717d0cf362c9890e692383c176579c37
skwak <skwak@0f5772cc-3132-cf4e-baf0-5355bf268a20>
2015-05-08 07:18:57 +0000

    split main to header and cpp files

--- trunk/fCap/jni/main.cpp
+++ trunk/fCap/jni/main.cpp
@@ -1,27 +1,4 @@
-#include "ace/Task.h"
-
-class CCapTask : public ACE_Task<ACE_MT_SYNCH>
-{
-public:
-  CCapTask();
-  ~CCapTask();
-
-  virtual int svc(void); 
-};
-
-CCapTask::CCapTask()
-{
-}
-
-CCapTask::~CCapTask()
-{
-}
-
-int CCapTask::svc(void)
-{
-  ACE_DEBUG((LM_DEBUG,"(%t) svc end\n"));
-  return 0;
-}
+#include "CCapTask.h"
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
