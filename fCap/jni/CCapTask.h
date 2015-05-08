b922dbe413ca001fd0acb0e5cc5b617ab5ba6a66
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-05-08 09:39:46 +0000

    fbinfo, get_surface_info ported

--- fCap/jni/CCapTask.h
+++ fCap/jni/CCapTask.h
@@ -2,12 +2,14 @@
 #define __CCAPTASK_H__
 
 #include "ace/Task.h"
+struct fbinfo;
 
 class CCapTask : public ACE_Task<ACE_MT_SYNCH>
 {
 public:
   CCapTask();
   ~CCapTask();
+   int get_surface_info(fbinfo& fbi,const int width, const int height, const int format);
 
   virtual int svc(void); 
 };
