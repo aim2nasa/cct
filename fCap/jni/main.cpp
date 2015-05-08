4db4fc5db57f6bfa16806a3332c9e73dbd4eb4d3
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-05-08 08:25:09 +0000

    CCapTask thread is waiting until it receives MB_HANGUP from the main thread

--- fCap/jni/main.cpp
+++ fCap/jni/main.cpp
@@ -1,4 +1,5 @@
 #include "CCapTask.h"
+#include "ace/OS_NS_unistd.h"
 
 int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
 {
@@ -7,6 +8,12 @@ int ACE_TMAIN(int argc, ACE_TCHAR *argv[])
   CCapTask cap;
   cap.activate(); 
 
+  ACE_Message_Block* hangup;
+  ACE_NEW_RETURN(hangup,ACE_Message_Block(0,ACE_Message_Block::MB_HANGUP),-1);
+
+  ACE_OS::sleep(5); //for 5 seconds
+
+  cap.putq(hangup);
   cap.wait();
   ACE_DEBUG((LM_DEBUG,"(%t) end of main\n"));
   return 0;
