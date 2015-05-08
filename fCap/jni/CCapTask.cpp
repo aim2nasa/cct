43eed7c04f3a154b062279c495fa7f790b083af7
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-05-08 08:54:03 +0000

    let screencap activated through popen and parses the received stream header

--- fCap/jni/CCapTask.cpp
+++ fCap/jni/CCapTask.cpp
@@ -12,6 +12,15 @@ int CCapTask::svc(void)
 {
   ACE_DEBUG((LM_DEBUG,"(%t) svc start\n"));
 
+  FILE* fp = popen("screencap","r");
+  if(!fp) ACE_ERROR_RETURN((LM_ERROR,"%p\n","popen(screencap)"),-1); 
+
+  int w,h,f;
+  if(fread(&w,1,sizeof(w),fp)!=sizeof(w)) ACE_ERROR_RETURN((LM_ERROR,"%p\n","fread(w)"),-1);
+  if(fread(&h,1,sizeof(h),fp)!=sizeof(h)) ACE_ERROR_RETURN((LM_ERROR,"%p\n","fread(h)"),-1);
+  if(fread(&f,1,sizeof(f),fp)!=sizeof(f)) ACE_ERROR_RETURN((LM_ERROR,"%p\n","fread(f)"),-1);
+  ACE_DEBUG((LM_DEBUG,"(%t) w(%d) h(%d) f(%d)\n",w,h,f));
+
   ACE_Message_Block* message;
   for(;;){
     if(this->getq(message)==-1) ACE_ERROR_RETURN((LM_ERROR,"%p\n","getq"),-1);
@@ -22,6 +31,7 @@ int CCapTask::svc(void)
       break;
     }
   }
+  pclose(fp);
   ACE_DEBUG((LM_DEBUG,"(%t) svc end\n"));
   return 0;
 }
