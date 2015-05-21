756f5ad414a35bc30b87a708040bfb8a7ca42e69
skwak <skwak@0f5772cc-3132-cf4e-baf0-5355bf268a20>
2015-05-21 03:02:45 +0000

    parseHeader의 리턴값을 헤더의 크기로 리턴하도록 수정

--- trunk/fDump/jni/utility.h
+++ trunk/fDump/jni/utility.h
@@ -5,6 +5,6 @@
 #include "ace/SOCK_Stream.h"
 
 int get_frame(_u8* p, _u32 fbiSize, ACE_SOCK_Stream& client_stream);
-void parseHeader(const _u8* pBuffer, ACE_TCHAR* pTimeStamp, int nTimeStampSize, int* pWidth, int* pHeight, int* Length);
+int parseHeader(const _u8* pBuffer, ACE_TCHAR* pTimeStamp, int nTimeStampSize, int* pWidth, int* pHeight, int* Length);
 
 #endif
