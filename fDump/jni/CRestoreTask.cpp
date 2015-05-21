90f6172295361bf89e8dbb00678017798412d3f3
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-05-21 04:49:03 +0000

    macro를 별도의 파일로 정의하고 이를 사용 CRestoreTask에서 헤더를 파싱해서 넘겨져온 데이터를 검증

--- fDump/jni/CRestoreTask.cpp
+++ fDump/jni/CRestoreTask.cpp
@@ -1,9 +1,13 @@
 #include "CRestoreTask.h"
+#include "utility.h"
+#include "fMacro.h"
 
 int CRestoreTask::svc(void)
 {
 	ACE_DEBUG((LM_DEBUG, "(%t) CRestoreTask::svc start\n"));
 
+	ACE_TCHAR pcTime[TIMESTAMP_SIZE];	//pc에서 스트림을 수신한 PC기준 시간
+	ACE_TCHAR dvTime[TIMESTAMP_SIZE];	//단말에서 캡처하는 시점의 단말기준 시간
 	ACE_Message_Block *message;
 	for (;;){
 		if (this->getq(message) == -1) ACE_ERROR_RETURN((LM_ERROR, "%p\n", "getq"), -1);
@@ -14,8 +18,11 @@ int CRestoreTask::svc(void)
 			break;
 		}
 
-		ACE_DEBUG((LM_DEBUG,"{%d:%d}\n",message->size(),this->msg_queue()->message_count()));
+		int nWidth, nHeight, nLength;
+		int nGet = parseHeader(reinterpret_cast<const _u8*>(message->rd_ptr()), dvTime, pcTime, TIMESTAMP_SIZE, &nWidth, &nHeight, &nLength);
+		ACE_ASSERT(nGet == HEADER_SIZE+TIMESTAMP_SIZE);
 
+		ACE_DEBUG((LM_DEBUG, "S:C:R:w:h:l(%d:%s:%s:%d:%d:%d)\n", message->size(), dvTime, pcTime, nWidth, nHeight, nLength));
 		message->release();
 	}
 	ACE_DEBUG((LM_DEBUG, "(%t) CRestoreTask::svc end\n"));
---
+++ fDump/jni/fMacro.h
@@ -0,0 +1,12 @@
+#ifndef __FMACRO_H__
+#define __FMACRO_H__
+
+#define SIZE_BUF (1024*4)
+#define REF_WIDTH 1440
+#define REF_HEIGHT 2560
+#define REF_AREA (REF_WIDTH*REF_HEIGHT)
+#define RGBA_KIND  4
+#define TIMESTAMP_SIZE 27
+#define HEADER_SIZE 39	//Timestamp(27)+width(4)+height(4)+length(4)
+
+#endif
