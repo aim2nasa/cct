6a08039dd58c2ae893abe6fc37f02ebb072e618f
skwak <skwak@0f5772cc-3132-cf4e-baf0-5355bf268a20>
2015-05-21 04:31:31 +0000

    Timestamp가 추가된 헤더를 파싱하기 위한 함수 오버로딩

--- trunk/fDump/jni/utility.h
+++ trunk/fDump/jni/utility.h
@@ -6,5 +6,6 @@
 
 int get_frame(_u8* p, _u32 fbiSize, ACE_SOCK_Stream& client_stream);
 int parseHeader(const _u8* pBuffer, ACE_TCHAR* pTimeStamp, int nTimeStampSize, int* pWidth, int* pHeight, int* Length);
+int parseHeader(const _u8* pBuffer, ACE_TCHAR* pDvTimeStamp, ACE_TCHAR* pPcTimeStamp, int nTimeStampSize, int* pWidth, int* pHeight, int* pLength);
 
 #endif
