a8d3c2eef80761b21659c9c44e2c2a8d01e511b8
Rossi Kwak <26228531+aim2nasa@users.noreply.github.com>
2015-05-21 05:43:55 +0000

    restore함수 원형 추가 및 버퍼 준비

--- fDump/jni/CRestoreTask.h
+++ fDump/jni/CRestoreTask.h
@@ -2,13 +2,20 @@
 #define __CRESTORETASK_H__
 
 #include "ace/Task.h"
+#include "fType.h"
 
 class CRestoreTask : public ACE_Task < ACE_MT_SYNCH >
 {
 public:
+	CRestoreTask();
+	~CRestoreTask();
+
 	virtual int svc(void);
+	int restore(const _u8* compress_buffer, const _u32 compress_buffer_len, const _u16 width, const _u16 height, const _u32 bpp);
 
 protected:
+	_u8*	m_pDecompBuffer;
+	_u8*	m_pInvertBuffer;
 };
 
 #endif
