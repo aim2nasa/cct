17983e43954f1b696a2c8d7bde52c7d5ed4bb6f9
skwak <skwak@0f5772cc-3132-cf4e-baf0-5355bf268a20>
2015-05-22 02:19:06 +0000

    dos2unix

--- trunk/fDump/jni/Android.mk
+++ trunk/fDump/jni/Android.mk
@@ -10,10 +10,14 @@ include $(PREBUILT_SHARED_LIBRARY)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := fDump
-LOCAL_SRC_FILES := client.cpp
+LOCAL_SRC_FILES := client.cpp \
+		   CRestoreTask.cpp \
+		   utility.cpp \
 LOCAL_LDFLAGS:=-fPIE -pie
 LOCAL_SHARED_LIBRARIES := libACE
 LOCAL_LDLIBS += -lz
-LOCAL_C_INCLUDES += /home/skwak/ACE_wrappers
+LOCAL_C_INCLUDES += /home/skwak/ACE_wrappers \
+		    /home/skwak/cct/fDump/jni \
+		    /home/skwak/android-ndk-r8e/platforms/android-14/arch-arm/usr/include
 
 include $(BUILD_EXECUTABLE)
